cf7e081ea7b7d6acbb191b6c999b2c75230e7791
coloradoc <104806606+coloradoc@users.noreply.github.com>
2024-04-13 15:46:59 -0400

    merge

--- scripts/contentMain.js
+++ scripts/contentMain.js
@@ -13,24 +13,47 @@ const observer = new MutationObserver(function() {
     // Select the element
     
     const elementToDelete = document.querySelectorAll('#rendering-content');
+    const elementToDelete2 = document.querySelectorAll("#content > ytd-rich-shelf-renderer");
+    const elementToChange = document.querySelectorAll("#primary > ytd-rich-grid-renderer")
+    
+    //const text = document.querySelectorAll("#video-title") title
 
 
     // Check if the element exists
+    if (elementToChange) {
+      //change row
+      elementToChange.forEach(function(element){
+        element.style.setProperty("--ytd-rich-grid-posts-per-row", "3");
+        element.style.setProperty("--ytd-rich-grid-items-per-row", "3");
+       // element.hide()
+        console.log(element)
+      });
+      }
+
+    if (elementToDelete2) {
+      // Remove the element from the DOM
+      elementToDelete2.forEach(function(element){
+        element.style.display = "none"
+       // element.hide()
+        console.log(element)
+      });
+      }
+
     if (elementToDelete) {
         // Remove the element from the DOM
         elementToDelete.forEach(function(element){
           element.style.display = "none"
           element.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none"
-         // element.hide()
+          // Set the value of the CSS custom property directly on the document's root element
+
+          console.log("parent" + element.parentElement) //.setProperty("--ytd-rich-grid-posts-per-row", "2")
+  
           console.log(element)
         });
-        
-
         }
     else{
       console.log("ELEMENT DOES NOT EXIST")
     }
-    
 })
 observer.observe(document.body, { childList: true, subtree: true });
 
