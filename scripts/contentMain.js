c10516e24e4496bb9c8c8c9a8436d2bf1b2691b6
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2024-04-14 10:15:27 -0400

    Minor but important changes to contentMain

--- scripts/contentMain.js
+++ scripts/contentMain.js
@@ -1,62 +1,58 @@
-console.log("youtube extension is running")
+console.log("youtube extension: main")
 
 //querySelector constants
 //if the website changes and we have to chanage the queryselectors, these constants should make that easier
-
-qs_QueryExample = "html"
-
 
 
 const observer = new MutationObserver(function() {
   if (reformatURL(document.URL) !== "https://www.youtube.com/"){
     observer.disconnect();
   }
-  console.log("hello")
-  //advertisement?.remove()
-    // Select the element
-    
-    const elementToDelete = document.querySelectorAll('#rendering-content');
-    const elementToDelete2 = document.querySelectorAll("#content > ytd-rich-shelf-renderer");
-    const elementToChange = document.querySelectorAll("#primary > ytd-rich-grid-renderer")
-    
-    //const text = document.querySelectorAll("#video-title") title
+  //console.log("Mutation detected")
 
-
-    // Check if the element exists
-    if (elementToChange) {
-      //change row
-      elementToChange.forEach(function(element){
-        element.style.setProperty("--ytd-rich-grid-posts-per-row", "3");
-        element.style.setProperty("--ytd-rich-grid-items-per-row", "3");
-       // element.hide()
-        console.log(element)
-      });
-      }
-
-    if (elementToDelete2) {
-      // Remove the element from the DOM
-      elementToDelete2.forEach(function(element){
-        element.style.display = "none"
-       // element.hide()
-        console.log(element)
-      });
-      }
-
-    if (elementToDelete) {
-        // Remove the element from the DOM
-        elementToDelete.forEach(function(element){
-          element.style.display = "none"
-          element.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none"
-          // Set the value of the CSS custom property directly on the document's root element
-
-          console.log("parent" + element.parentElement) //.setProperty("--ytd-rich-grid-posts-per-row", "2")
+  // Select the element
+  const elementToDelete = document.querySelectorAll('#rendering-content');
+  const elementToDelete2 = document.querySelectorAll("#content > ytd-rich-shelf-renderer");
+  const elementToChange = document.querySelectorAll("#primary > ytd-rich-grid-renderer")
   
-          console.log(element)
-        });
-        }
-    else{
-      console.log("ELEMENT DOES NOT EXIST")
-    }
+  //const text = document.querySelectorAll("#video-title") title
+
+  // Check if the element exists
+  if (elementToChange) {
+    //change row
+    elementToChange.forEach(function(element){
+      element.style.setProperty("--ytd-rich-grid-posts-per-row", "3");
+      element.style.setProperty("--ytd-rich-grid-items-per-row", "3");
+      //console.log(element)
+    });
+  }
+
+  if (elementToDelete2) {
+    // Remove the element from the DOM
+    elementToDelete2.forEach(function(element){
+      if(element.style.display !== "none"){
+        element.style.display = "none"
+        console.log(element)
+      }
+    });
+  }
+
+  if (elementToDelete) {
+    // Remove the element from the DOM
+    elementToDelete.forEach(function(element){
+      if(element.parentElement.parentElement.parentElement.parentElement.parentElement.style.display !== "none"){
+        element.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none"
+        // Set the value of the CSS custom property directly on the document's root element
+
+        //console.log("parent" + element.parentElement) //.setProperty("--ytd-rich-grid-posts-per-row", "2")
+
+        console.log(element)
+      }
+    });
+  }
+  else{
+    console.log("ELEMENT DOES NOT EXIST")
+  }
 })
 observer.observe(document.body, { childList: true, subtree: true });
 
