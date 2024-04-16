a814f774fa34472366b8961b97dfb150ece6d550
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2024-04-16 15:37:09 -0400

    Added options page to popup

--- options/options.js
+++ options/options.js
@@ -17,9 +17,27 @@ async function main(){
         console.warn("Node name not detected: ", settingElement.nodeName)
       }
     }
+
+    //make each option automatically update itself instead of using a save button
+    settingElement.addEventListener("click", async function(){
+      // const changes = getChangedSettings()
+      // console.log(changes)
+      const state = (settingElement.checked ?? settingElement.value)
+      
+      let result = {}
+
+      result[option] = state
+        
+      await chrome.storage.local.set(result)
+      //testing
+      console.log(await chrome.storage.local.get(null))
+
+      //apply changes to current settings
+      settings[option] = result[option]
+    })
   }
 
-  //get which values the user changed
+  //get which option values differ from the current values
   function getChangedSettings(){
     let result = {}
 
@@ -38,34 +56,34 @@ async function main(){
   const saveButton = document.querySelector("#saveButton")
 
   //Save button functionality
-  saveButton.addEventListener("click", async function(){
-    saveButton.disabled = true
-    saveButton.textContent = "Saving"
+  // saveButton.addEventListener("click", async function(){
+  //   saveButton.disabled = true
+  //   saveButton.textContent = "Saving"
 
-    const changes = getChangedSettings()
-    console.log(changes)
+  //   const changes = getChangedSettings()
+  //   console.log(changes)
     
-    //only run code if any changes were made
-    if(Object.keys(changes).length > 0){
+  //   //only run code if any changes were made
+  //   if(Object.keys(changes).length > 0){
         
-      //Send message to service worker
-      if(changes.theme){
-        chrome.runtime.sendMessage({action: "changeTheme", details: changes.theme})
-      }
+  //     //Send message to service worker
+  //     if(changes.theme){
+  //       chrome.runtime.sendMessage({action: "changeTheme", details: changes.theme})
+  //     }
       
-      await chrome.storage.local.set(changes)
-      //testing
-      //console.log(await chrome.storage.local.get(null))
+  //     await chrome.storage.local.set(changes)
+  //     //testing
+  //     //console.log(await chrome.storage.local.get(null))
 
-      //apply changes to current settings
-      for(const change in changes){
-        settings[change] = changes[change]
-      }
-    }
+  //     //apply changes to current settings
+  //     for(const change in changes){
+  //       settings[change] = changes[change]
+  //     }
+  //   }
 
-    saveButton.disabled = false
-    saveButton.textContent = "Save"
-  })
+  //   saveButton.disabled = false
+  //   saveButton.textContent = "Save"
+  // })
 }
 
 main()
